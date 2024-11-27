3346253c730d5bccc662a5e19d78b06f0fec572e
SadraSamadzadeh <sadra.samadzadeh1381@gmail.com>
2024-11-27 23:41:26 +0100

    added sentry for error management

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a333a21a8b0e5cbc4154f1be68e9b526@o4508372588756992.ingest.de.sentry.io/4508372596031568",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
